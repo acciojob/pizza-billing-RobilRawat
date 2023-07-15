031e475be0de05d470dcc7d88a83127b4eaa0193
RobilRawat <robil.cs.coder@gmail.com>
2023-07-15 22:24:00 +0530

    third commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,21 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+
+  boolean Ealready= false;
+    boolean Talready= false;
+    boolean Balready= false;
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg==true){
+            this.price= 300;
+
+        }else{
+            this.price= 400;
+        }
     }
 
     public int getPrice(){
@@ -17,18 +29,42 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(Ealready==false){
+            price+=80;
+          Ealready=true;
+        }
+
+
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+
+        if(Talready==false){
+            if(isVeg){
+                price+= 70;
+            }else{
+                price+=120;
+            }
+            Talready= true;
+        }
+
+
+
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(Balready==false){
+            price+=20;
+            Balready= true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+            bill= Integer.toString(price);
         return this.bill;
     }
 }
